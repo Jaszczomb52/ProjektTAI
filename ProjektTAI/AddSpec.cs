6559a001af4fd60e2adeca68971b9b7f7c295019
Jaszczomb52 <cmdr.blackburn@gmail.com>
2022-06-20 21:34:03 +0200

    Zrobione otwieranie okna dodania specjalizacji. Przyciski Specjalizacje.cs działają, do przycisku usuwania należy dodać link z API który dopiero należy stworzyć.

---
+++ ProjektTAI/AddSpec.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjektTAI
+{
+    public partial class AddSpec : Form
+    {
+        public AddSpec(Emplo em, bool modify)
+        {
+            InitializeComponent();
+            Visible = true;
+        }
+    }
+}
--- ProjektTAI/Employees.Designer.cs
+++ ProjektTAI/Employees.Designer.cs
@@ -34,6 +34,7 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -43,9 +44,9 @@
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(3, 59);
+            this.button1.Location = new System.Drawing.Point(3, 64);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(228, 50);
+            this.button1.Size = new System.Drawing.Size(228, 55);
             this.button1.TabIndex = 0;
             this.button1.Text = "Edytuj pracownika";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,7 +58,7 @@
             this.button2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button2.Location = new System.Drawing.Point(3, 3);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(228, 50);
+            this.button2.Size = new System.Drawing.Size(228, 55);
             this.button2.TabIndex = 1;
             this.button2.Text = "Dodaj pracownika";
             this.button2.UseVisualStyleBackColor = true;
@@ -67,9 +68,9 @@
             // 
             this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button3.Location = new System.Drawing.Point(3, 115);
+            this.button3.Location = new System.Drawing.Point(3, 125);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(228, 52);
+            this.button3.Size = new System.Drawing.Size(228, 55);
             this.button3.TabIndex = 2;
             this.button3.Text = "Usuń pracownika";
             this.button3.UseVisualStyleBackColor = true;
@@ -97,15 +98,17 @@
             this.tableLayoutPanel2.Controls.Add(this.button3, 0, 2);
             this.tableLayoutPanel2.Controls.Add(this.button1, 0, 1);
             this.tableLayoutPanel2.Controls.Add(this.button2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button4, 0, 3);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(563, 3);
             this.tableLayoutPanel2.MinimumSize = new System.Drawing.Size(234, 122);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 3;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(234, 170);
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(234, 246);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
             // dataGridView1
@@ -120,6 +123,18 @@
             this.dataGridView1.RowTemplate.Height = 25;
             this.dataGridView1.Size = new System.Drawing.Size(554, 442);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(3, 186);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(228, 57);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Edytuj specjalizacje";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Employees
             // 
@@ -145,5 +160,6 @@
         private TableLayoutPanel tableLayoutPanel1;
         private TableLayoutPanel tableLayoutPanel2;
         private DataGridView dataGridView1;
+        private Button button4;
     }
 }
